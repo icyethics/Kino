5111a087aa7f27e1ea83e777fd7c4a0b2e2b1cc4
icyethics <97385843+icyethics@users.noreply.github.com>
2025-08-05 15:50:32 +0200

    commented out unneccessary code

--- src/run_info.lua
+++ src/run_info.lua
@@ -170,134 +170,134 @@ end
 
 
 
-function create_UIBox_your_collection_jokers()
-  local deck_tables = {}
+-- function create_UIBox_your_collection_jokers()
+--   local deck_tables = {}
 
-  G.your_collection = {}
-  for j = 1, 3 do
-    G.your_collection[j] = CardArea(
-      G.ROOM.T.x + 0.2*G.ROOM.T.w/2,G.ROOM.T.h,
-      5*G.CARD_W,
-      0.95*G.CARD_H, 
-      {card_limit = 5, type = 'title', highlight_limit = 0, collection = true})
-    table.insert(deck_tables, 
-    {n=G.UIT.R, config={align = "cm", padding = 0.07, no_fill = true}, nodes={
-      {n=G.UIT.O, config={object = G.your_collection[j]}}
-    }}
-    )
-  end
+--   G.your_collection = {}
+--   for j = 1, 3 do
+--     G.your_collection[j] = CardArea(
+--       G.ROOM.T.x + 0.2*G.ROOM.T.w/2,G.ROOM.T.h,
+--       5*G.CARD_W,
+--       0.95*G.CARD_H, 
+--       {card_limit = 5, type = 'title', highlight_limit = 0, collection = true})
+--     table.insert(deck_tables, 
+--     {n=G.UIT.R, config={align = "cm", padding = 0.07, no_fill = true}, nodes={
+--       {n=G.UIT.O, config={object = G.your_collection[j]}}
+--     }}
+--     )
+--   end
 
-  local joker_options = {}
-  for i = 1, math.ceil(#G.P_CENTER_POOLS.Joker/(5*#G.your_collection)) do
-    table.insert(joker_options, localize('k_page')..' '..tostring(i)..'/'..tostring(math.ceil(#G.P_CENTER_POOLS.Joker/(5*#G.your_collection))))
-  end
+--   local joker_options = {}
+--   for i = 1, math.ceil(#G.P_CENTER_POOLS.Joker/(5*#G.your_collection)) do
+--     table.insert(joker_options, localize('k_page')..' '..tostring(i)..'/'..tostring(math.ceil(#G.P_CENTER_POOLS.Joker/(5*#G.your_collection))))
+--   end
 
-  for i = 1, 5 do
-    for j = 1, #G.your_collection do
-      local center = G.P_CENTER_POOLS["Joker"][i+(j-1)*5]
-      local card = Card(G.your_collection[j].T.x + G.your_collection[j].T.w/2, G.your_collection[j].T.y, G.CARD_W, G.CARD_H, nil, center)
-      card.sticker = get_joker_win_sticker(center)
-      G.your_collection[j]:emplace(card)
-    end
-  end
+--   for i = 1, 5 do
+--     for j = 1, #G.your_collection do
+--       local center = G.P_CENTER_POOLS["Joker"][i+(j-1)*5]
+--       local card = Card(G.your_collection[j].T.x + G.your_collection[j].T.w/2, G.your_collection[j].T.y, G.CARD_W, G.CARD_H, nil, center)
+--       card.sticker = get_joker_win_sticker(center)
+--       G.your_collection[j]:emplace(card)
+--     end
+--   end
 
-  INIT_COLLECTION_CARD_ALERTS()
+--   INIT_COLLECTION_CARD_ALERTS()
   
-  local t =  create_UIBox_generic_options({ back_func = 'your_collection', contents = {
-        {n=G.UIT.R, config={align = "cm", r = 0.1, colour = G.C.BLACK, emboss = 0.05}, nodes=deck_tables}, 
-        {n=G.UIT.R, config={align = "cm"}, nodes={
-          create_option_cycle({options = joker_options, w = 4.5, cycle_shoulders = true, opt_callback = 'your_collection_joker_page', current_option = 1, colour = G.C.RED, no_pips = true, focus_args = {snap_to = true, nav = 'wide'}})
-        }}
-    }})
-  return t
-end
+--   local t =  create_UIBox_generic_options({ back_func = 'your_collection', contents = {
+--         {n=G.UIT.R, config={align = "cm", r = 0.1, colour = G.C.BLACK, emboss = 0.05}, nodes=deck_tables}, 
+--         {n=G.UIT.R, config={align = "cm"}, nodes={
+--           create_option_cycle({options = joker_options, w = 4.5, cycle_shoulders = true, opt_callback = 'your_collection_joker_page', current_option = 1, colour = G.C.RED, no_pips = true, focus_args = {snap_to = true, nav = 'wide'}})
+--         }}
+--     }})
+--   return t
+-- end
 
-function G.UIDEF.used_vouchers()
+-- function G.UIDEF.used_vouchers()
 
-  local silent = false
-  local keys_used = {}
-  local area_count = 0
-  local voucher_areas = {}
-  local voucher_tables = {}
-  local voucher_table_rows = {}
+--   local silent = false
+--   local keys_used = {}
+--   local area_count = 0
+--   local voucher_areas = {}
+--   local voucher_tables = {}
+--   local voucher_table_rows = {}
 
-  for k, v in ipairs(G.P_CENTER_POOLS["Voucher"]) do
-    local key = 1 + math.floor((k-0.1)/2)
-    keys_used[key] = keys_used[key] or {}
-    if G.GAME.used_vouchers[v.key] then 
-      keys_used[key][#keys_used[key]+1] = v
-    end
-  end
+--   for k, v in ipairs(G.P_CENTER_POOLS["Voucher"]) do
+--     local key = 1 + math.floor((k-0.1)/2)
+--     keys_used[key] = keys_used[key] or {}
+--     if G.GAME.used_vouchers[v.key] then 
+--       keys_used[key][#keys_used[key]+1] = v
+--     end
+--   end
 
-  for k, v in ipairs(keys_used) do 
-    if next(v) then
-      area_count = area_count + 1
-    end
-  end
+--   for k, v in ipairs(keys_used) do 
+--     if next(v) then
+--       area_count = area_count + 1
+--     end
+--   end
 
-  for k, v in ipairs(keys_used) do 
-    if next(v) then
+--   for k, v in ipairs(keys_used) do 
+--     if next(v) then
 
-		if #voucher_areas == 5 or #voucher_areas == 10 then 
-			table.insert(voucher_table_rows, 
-			{n=G.UIT.R, config={align = "cm", padding = 0, no_fill = true}, nodes=voucher_tables}
-			)
-			voucher_tables = {}
-		end
+-- 		if #voucher_areas == 5 or #voucher_areas == 10 then 
+-- 			table.insert(voucher_table_rows, 
+-- 			{n=G.UIT.R, config={align = "cm", padding = 0, no_fill = true}, nodes=voucher_tables}
+-- 			)
+-- 			voucher_tables = {}
+-- 		end
 
-		voucher_areas[#voucher_areas + 1] = CardArea(
-				G.ROOM.T.x + 0.2*G.ROOM.T.w/2,G.ROOM.T.h,
-				(#v == 1 and 1 or 1.33)*G.CARD_W,
-				(area_count >=10 and 0.75 or 1.07)*G.CARD_H, 
-				{card_limit = 2, type = 'voucher', highlight_limit = 0}
-		)
+-- 		voucher_areas[#voucher_areas + 1] = CardArea(
+-- 				G.ROOM.T.x + 0.2*G.ROOM.T.w/2,G.ROOM.T.h,
+-- 				(#v == 1 and 1 or 1.33)*G.CARD_W,
+-- 				(area_count >=10 and 0.75 or 1.07)*G.CARD_H, 
+-- 				{card_limit = 2, type = 'voucher', highlight_limit = 0}
+-- 		)
 
-		for kk, vv in ipairs(v) do 
-			local center = G.P_CENTERS[vv.key]
-			local card = Card(
-				voucher_areas[#voucher_areas].T.x + voucher_areas[#voucher_areas].T.w/2, 
-				voucher_areas[#voucher_areas].T.y, 
-				G.CARD_W, 
-				G.CARD_H, 
-				nil, 
-				center, 
-				{
-					bypass_discovery_center=true,
-					bypass_discovery_ui=true,
-					bypass_lock=true
-				}
-			)
-			card.ability.order = vv.order
-			card:start_materialize(nil, silent)
-			silent = true
-			voucher_areas[#voucher_areas]:emplace(card)
+-- 		for kk, vv in ipairs(v) do 
+-- 			local center = G.P_CENTERS[vv.key]
+-- 			local card = Card(
+-- 				voucher_areas[#voucher_areas].T.x + voucher_areas[#voucher_areas].T.w/2, 
+-- 				voucher_areas[#voucher_areas].T.y, 
+-- 				G.CARD_W, 
+-- 				G.CARD_H, 
+-- 				nil, 
+-- 				center, 
+-- 				{
+-- 					bypass_discovery_center=true,
+-- 					bypass_discovery_ui=true,
+-- 					bypass_lock=true
+-- 				}
+-- 			)
+-- 			card.ability.order = vv.order
+-- 			card:start_materialize(nil, silent)
+-- 			silent = true
+-- 			voucher_areas[#voucher_areas]:emplace(card)
 
-		end
-		table.insert(voucher_tables, 
-		{n=G.UIT.C, config={align = "cm", padding = 0, no_fill = true}, nodes={
-			{n=G.UIT.O, config={object = voucher_areas[#voucher_areas]}}
-		}}
-		)
-		end
-	end
-  table.insert(voucher_table_rows,
-          {n=G.UIT.R, config={align = "cm", padding = 0, no_fill = true}, nodes=voucher_tables}
-        )
+-- 		end
+-- 		table.insert(voucher_tables, 
+-- 		{n=G.UIT.C, config={align = "cm", padding = 0, no_fill = true}, nodes={
+-- 			{n=G.UIT.O, config={object = voucher_areas[#voucher_areas]}}
+-- 		}}
+-- 		)
+-- 		end
+-- 	end
+--   table.insert(voucher_table_rows,
+--           {n=G.UIT.R, config={align = "cm", padding = 0, no_fill = true}, nodes=voucher_tables}
+--         )
 
   
-  local t = silent and {n=G.UIT.ROOT, config={align = "cm", colour = G.C.CLEAR}, nodes=
-  {
-    {n=G.UIT.R, config={align = "cm"}, nodes={
-      {n=G.UIT.O, config={object = DynaText({string = {localize('ph_vouchers_redeemed')}, colours = {G.C.UI.TEXT_LIGHT}, bump = true, scale = 0.6})}}
-    }},
-    {n=G.UIT.R, config={align = "cm", minh = 0.5}, nodes={
-    }},
-    {n=G.UIT.R, config={align = "cm", colour = G.C.BLACK, r = 1, padding = 0.15, emboss = 0.05}, nodes={
-      {n=G.UIT.R, config={align = "cm"}, nodes=voucher_table_rows},
-    }}
-  }} or 
-  {n=G.UIT.ROOT, config={align = "cm", colour = G.C.CLEAR}, nodes={
-    {n=G.UIT.O, config={object = DynaText({string = {localize('ph_no_vouchers')}, colours = {G.C.UI.TEXT_LIGHT}, bump = true, scale = 0.6})}}
-  }}
-  return t
-end
+--   local t = silent and {n=G.UIT.ROOT, config={align = "cm", colour = G.C.CLEAR}, nodes=
+--   {
+--     {n=G.UIT.R, config={align = "cm"}, nodes={
+--       {n=G.UIT.O, config={object = DynaText({string = {localize('ph_vouchers_redeemed')}, colours = {G.C.UI.TEXT_LIGHT}, bump = true, scale = 0.6})}}
+--     }},
+--     {n=G.UIT.R, config={align = "cm", minh = 0.5}, nodes={
+--     }},
+--     {n=G.UIT.R, config={align = "cm", colour = G.C.BLACK, r = 1, padding = 0.15, emboss = 0.05}, nodes={
+--       {n=G.UIT.R, config={align = "cm"}, nodes=voucher_table_rows},
+--     }}
+--   }} or 
+--   {n=G.UIT.ROOT, config={align = "cm", colour = G.C.CLEAR}, nodes={
+--     {n=G.UIT.O, config={object = DynaText({string = {localize('ph_no_vouchers')}, colours = {G.C.UI.TEXT_LIGHT}, bump = true, scale = 0.6})}}
+--   }}
+--   return t
+-- end
